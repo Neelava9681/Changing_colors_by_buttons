e1bf773b1c43bddff8b7fbf7698806f3bb2362b6
NeelavaHBR <hbr.neelava@gmail.com>
2024-03-28 22:43:18 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+import React,{useEffect, useState} from 'react';
 import './App.css';
 
 function App() {
+  const [bgColor, setbgColor] = useState({})
+
+  const click = (color) =>{
+    setbgColor(color)
+  }
+
+  useEffect(()=>{
+    document.body.style.backgroundColor =bgColor
+  },[bgColor])
+
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={()=>click("red")} style={{backgroundColor:"red", color:"black",marginTop:"30px",height:"50px", width:"80px", marginRight:"5px"}}>
+        <h1 style={{margin:"5px"}}>red</h1>
+      </button>
+      <button onClick={()=>click("blue")} style={{backgroundColor:"blue", color:"black",marginTop:"30px",height:"50px", width:"80px", marginRight:"5px"}}>
+        <h1 style={{margin:"5px"}}>blue</h1>
+      </button>
+      <button onClick={()=>click("Yellow")} style={{backgroundColor:"yellow", color:"black",marginTop:"30px",height:"50px", width:"auto", marginRight:"5px"}}>
+        <h1 style={{margin:"5px"}}>yellow</h1>
+      </button>
+      <button onClick={()=>click("cyan")} style={{backgroundColor:"cyan", color:"black",marginTop:"30px",height:"50px", width:"auto", marginRight:"5px"}}>
+        <h1 style={{margin:"5px"}}>cyan</h1>
+      </button>
+      <button onClick={()=>click("purple")} style={{backgroundColor:"purple", color:"black",marginTop:"30px",height:"50px", width:"auto", marginRight:"5px"}}>
+        <h1 style={{margin:"5px"}}>purple</h1>
+      </button>
     </div>
   );
 }
